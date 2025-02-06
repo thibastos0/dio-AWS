ca8bd1f2f476d5e2c0db5e4f0e45a80f382a111c
thibastos0 <thibastos@live.com>
2025-02-06 11:00:38 -0300

    atividade versão 1

--- aws_textract_teste.py
+++ aws_textract_teste.py
@@ -3,36 +3,41 @@ from pathlib import Path
 
 import boto3
 from botocore.exceptions import ClientError
-#from mypy_boto3_textract.type_defs import DetectDocumentTextResponseTypeDef
-
 
 def detect_file_text() -> None:
-    client = boto3.client("textract")
-
+    textract = boto3.client("textract")
+    
+    # Document
     file_path = str(Path(__file__).parent / "images" / "lista-material-escolar.jpeg")
-    with open(file_path, "rb") as f:
-        document_bytes = f.read()
+    
+    # Re1oad document content
+    with open(file_path, "rb") as file:
+        # document_bytes = file.read() # aula
+        imageBytes = bytearray(file.read())
 
     try:
-        response = client.detect_document_text(Document={"Bytes": document_bytes})
+        # Call Amazon Textract
+        # response = textract.detect_document_text(Document={"Bytes": document_bytes}) # aula
+        response = textract.detect_document_text(Document={'Bytes': imageBytes})
         with open("response.json", "w") as response_file:
             response_file.write(json.dumps(response))
+
     except ClientError as e:
         print(f"Erro processando documento: {e}")
 
-
 def get_lines() -> list[str]:
     try:
+        # print detected text
+    
         with open("response.json", "r") as f:
-            # data: DetectDocumentTextResponseTypeDef = json.loads(f.read())
             data = json.loads(f.read())
             blocks = data["Blocks"]
-        return [block["Text"] for block in blocks if block["BlockType"] == "LINE"]  # type: ignore
+        
+            return [block["Text"] for block in blocks if block["BlockType"] == "LINE"]
     except IOError:
         detect_file_text()
     return []
 
 
-if __name__ == "__main__":
-    for line in get_lines():
-        print(line)
+for line in get_lines():
+    print(line)
